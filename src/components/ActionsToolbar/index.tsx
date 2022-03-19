80921726b9b3e502dd302fb1493cc2488b8e9b16
E19B7W <thales.gelinger@ehi.com>
2022-03-19 02:41:45 -0300

    feat: add wire connection

--- src/components/ActionsToolbar/index.tsx
+++ src/components/ActionsToolbar/index.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import { Icon } from "../Icon";
 
 import styles from "./styles.module.scss";
 
 const ICON_DEFAULT_SIZE = 40;
 
-export const ActionsToolbar = () => {
+type ActionTypes = "edit" | "";
+interface ActionsToolbarProps {
+  onActionChange: (actionType: ActionTypes) => void;
+}
+
+export const ActionsToolbar = ({ onActionChange }: ActionsToolbarProps) => {
+  const [currentAction, setCurrentAction] = useState<ActionTypes>("");
+
+  useEffect(() => {
+    onActionChange(currentAction);
+  }, [currentAction]);
+
+  const handleActionsChange = (actionType: ActionTypes) => () => {
+    setCurrentAction(actionType === currentAction ? "" : actionType);
+  };
+
   return (
     <section className={styles.container}>
       <Icon name="play" size={ICON_DEFAULT_SIZE} color={"#black"} />
-      <Icon name="edit" size={ICON_DEFAULT_SIZE} color={"#black"} />
+      <Icon
+        name="edit"
+        size={ICON_DEFAULT_SIZE}
+        color={"#black"}
+        onClick={handleActionsChange("edit")}
+      />
       <Icon name="print" size={ICON_DEFAULT_SIZE} color={"#black"} />
       <Icon name="share" size={ICON_DEFAULT_SIZE} color={"#black"} />
       <Icon name="trash" size={ICON_DEFAULT_SIZE} color={"#black"} />
--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,15 +1,16 @@
+import { ButtonHTMLAttributes } from "react";
 import { icons } from "./icons";
 import styles from "./styles.module.scss";
 
-interface IconProps {
+interface IconProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   name: keyof typeof icons;
   size: number;
   color: string;
 }
 
-export const Icon = ({ name, size, color }: IconProps) => {
+export const Icon = ({ name, size, color, ...rest }: IconProps) => {
   return (
-    <button className={styles.container}>
+    <button className={styles.container} {...rest}>
       <img height={size} width={size} color={color} src={icons[name]} />
     </button>
   );
