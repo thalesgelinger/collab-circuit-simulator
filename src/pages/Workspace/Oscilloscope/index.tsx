187d248b0e9de69ccf78a37c819508126dfc3d58
Henrique Santos <henriquegs.contato@gmail.com>
2022-05-03 20:56:56 -0300

    feature: install chart

--- src/pages/Workspace/Oscilloscope/index.tsx
+++ src/pages/Workspace/Oscilloscope/index.tsx
@@ -1,12 +1,5 @@
 import { useState } from "react";
-import {
-  Chart,
-  LineController,
-  LineElement,
-  PointElement,
-  LinearScale,
-  Title,
-} from "chart.js";
+import { Bar } from "react-chartjs-2";
 import styles from "./styles.module.scss";
 
 export const Oscilloscope = () => {
@@ -21,7 +14,48 @@ export const Oscilloscope = () => {
         >
           Ampliar
         </button>
-        {oscilloscopeOn && <div className={styles.display}>oiii</div>}
+        {oscilloscopeOn && (
+          <div className={styles.display}>
+            <Bar
+              typeof="bar"
+              data={{
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [
+                  {
+                    label: "# of Votes",
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                      "rgba(255, 206, 86, 0.2)",
+                      "rgba(75, 192, 192, 0.2)",
+                      "rgba(153, 102, 255, 0.2)",
+                      "rgba(255, 159, 64, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                      "rgba(255, 206, 86, 1)",
+                      "rgba(75, 192, 192, 1)",
+                      "rgba(153, 102, 255, 1)",
+                      "rgba(255, 159, 64, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+              height={400}
+              width={1200}
+              options={{
+                scales: {
+                  y: {
+                    beginAtZero: true,
+                  },
+                },
+              }}
+            />
+          </div>
+        )}
       </div>
     </section>
   );
